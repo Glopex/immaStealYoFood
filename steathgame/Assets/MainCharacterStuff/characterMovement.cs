f7c031b7f0c455d03c90ae7cd14ed2a7f032f215
Glopex <62385400+Glopex@users.noreply.github.com>
2022-02-18 17:51:29 +0000

    the boy is shmoovin

--- steathgame/Assets/MainCharacterStuff/characterMovement.cs
+++ steathgame/Assets/MainCharacterStuff/characterMovement.cs
@@ -1,50 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 
 public class characterMovement : MonoBehaviour
 {
-    GameObject Player;
-    Vector3 PlayerPosition;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    private Vector3 _input;
+
+    private void Update()
     {
-        Player = gameObject;
-        PlayerPosition = Player.transform.position;
+        GatherInput();
+        Look();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        Player.transform.position = PlayerPosition;
+        Move();
     }
 
-    public void OnUp()
+    private void GatherInput()
     {
-        PlayerPosition.x += 1; 
-        PlayerPosition.z += 1;
-        /*var v = input;
-        Debug.Log(v);
-        Vector2 inputVec = input.Get<Vector2>();
-        PlayerPosition = new Vector3(inputVec.x, 0, inputVec.y);*/
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
-    public void OnDown()
-    {
-        PlayerPosition.x -= 1;
-        PlayerPosition.z -= 1;
-        
-    }
-    public void OnLeft()
-    {
-        PlayerPosition.x -= 1;
-        PlayerPosition.z += 1;
 
-    }
-    public void OnRight()
+    private void Look()
     {
-        PlayerPosition.x += 1;
-        PlayerPosition.z -= 1;
+        if (_input == Vector3.zero) return;
 
+        var rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
     }
+
+    private void Move()
+    {
+        _rb.MovePosition(transform.position + transform.forward * _input.normalized.magnitude * _speed * Time.deltaTime);
+    }
+}
+
+public static class Helpers
+{
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
 }
